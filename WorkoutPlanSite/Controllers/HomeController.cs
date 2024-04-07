8a5aee64aff12e78eb59114b499bfecf825ca186
Ivan Zlatinov <vani.zlatinov51@gmail.com>
2024-04-07 23:05:57 +0300

    Modified Explore Page

--- WorkoutPlanSite/Controllers/HomeController.cs
+++ WorkoutPlanSite/Controllers/HomeController.cs
@@ -1,19 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Numerics;
 using WorkoutPlanSite.Data;
+using WorkoutPlanSite.Data.Data.Models.Enums;
 using WorkoutPlanSite.Models;
+using WorkoutPlanSite.Models.Equipment;
+using WorkoutPlanSite.Models.Exercise;
+using WorkoutPlanSite.Services.DTOs;
+using WorkoutPlanSite.Services.Interfaces;
+using WorkoutPlanSite.Services.Services;
 
 namespace WorkoutPlanSite.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext context;
-        
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        private readonly IEquipmentService equipmentService;
+        private readonly IExerciseService exerciseService;
+        public HomeController(ILogger<HomeController> logger,
+            IExerciseService exerciseService, IEquipmentService equipmentService)
         {
             _logger = logger;
-            this.context = context;
+            this.equipmentService = equipmentService;
+            this.exerciseService = exerciseService;
         }
 
         public async Task<IActionResult> Index()
@@ -21,10 +30,56 @@ namespace WorkoutPlanSite.Controllers
             return View();
         }
 
-
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult Explore()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Equipments(string? plan)
+        {
+            IEnumerable<EquipmentDTO> equipments = await equipmentService.GetAllAsync();
+            IEnumerable<EquipmentViewModel> views = equipments.Select(e => new EquipmentViewModel
+            {
+                Plan = e.Plan.ToString(),
+                Id = e.Id,
+                Name = e.Name,
+                Weight = e.Weight,
+                Type = e.Type.Name,
+                ImageUrl = e.ImageUrl
+            });
+
+            if (plan != null)
+            {
+                views = views.Where(e => e.Plan == plan);
+            }
+
+            return View(views);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Exercises()
+        {
+            IEnumerable<ExerciseDTO> exercises = await exerciseService.GetAllAsync();
+            ExerciseViewModel[] exerciseViews = exercises.Select(x => new ExerciseViewModel()
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Description = x.Description,
+                Sets = x.Sets,
+                Repetitions = x.Repetitions,
+                Difficulty = x.difficulty.ToString(),
+                EquipmentId = x.EquipmentId,
+                EquipmentName = x.Equipment.Name,
+                ImageURL = x.ImageURL,
+            })
+            .ToArray();
+
+            return View(exerciseViews);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -32,5 +87,6 @@ namespace WorkoutPlanSite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
